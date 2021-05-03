25b9f54a87d924468f786e4adc4860705828a7b5
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2021-05-04 01:35:28 +0200

    BOs fertig gestellt

---
+++ src/server/bo/LearnProfile.py
@@ -0,0 +1,59 @@
+from server.bo.NamedBusinessObject import NamedBusinessObject as nbo
+
+class LearnProfile(nbo):
+    def __init__(self):
+        super().__init__()
+        self._study_status = 0
+        self._frequency = None
+        self._prev_knowledge = ""
+        self._extroversion = 0
+        self._profile_id = None
+
+
+    def set_study_status(self, study_status):
+        self._study_status = study_status
+
+    def get_study_status(self):
+        return self.study_status
+
+    def set_frequency(self, frequency):
+        self._frequency = frequency 
+
+    def get_frequency(self):
+        return self._frequency
+
+    def set_prev_knowledge(self, prev_knowledge):
+        self._prev_knowledge = prev_knowledge
+
+    def get_prev_knowledge(self):
+        return self._prev_knowledge
+
+    def set_extroversion(self, extroversion ):
+        self._extroversion = extroversion
+
+    def get_extroversion(self):
+        return self._extroversion
+
+    def set_profile_id(self, profile_id):
+        self._profile_id = profile_id
+
+    def get_profile_id(self):
+        return self._profile_id
+
+
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in ein Rating()."""
+        obj = LearnProfile()
+        """print(dictionary)"""
+        obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
+        obj.set_creation_time(dictionary["creation_time"])
+        obj.set_name(dictionary["name"]) 
+        obj.set_study_status(dictionary["study_status"])
+        obj.set_frequency(dictionary["frequency"])
+        obj.set_prev_knowledge(dictionary["prev_knowledge"])
+        obj.set_extroversion(dictionary["extroversion"])
+        obj.set_profile_id(dictionary["profile_id"])
+
+        return obj
---
+++ src/server/bo/Profile.py
@@ -0,0 +1,68 @@
+from server.bo.NamedBusinessObject import NamedBusinessObject as nbo
+
+class Profile(nbo):
+    def __init__(self):
+        super().__init__()
+        self._age = None
+        self._adress = ""
+        self._semester = None
+        self._degree_course = ""
+        self._pre_knowledge = ""
+        self._person_id = None
+    
+
+    def set_age(self, age):
+        self._age = age
+
+    def get_age(self):
+        return self._age
+
+    def set_adress(self, adress):
+        self._adress = adress
+
+    def get_adress(self):
+        return self._adress
+    
+    def set_semester(self, semester):
+        self._semester = semester
+
+    def get_semester(self):
+        return self._semester
+
+    def set_degree_course(self, degree_course):
+        self._degree_course = degree_course
+
+    def get_degree_course(self):
+        return self._degree_course   
+
+    def set_pre_knowledge(self, pre_knowledge):
+        self._pre_knowledge = pre_knowledge
+
+    def get_pre_knowledge(self):
+        return self._pre_knowledge
+
+    def set_person_id(self, person_id):
+        self._person_id = person_id
+
+    def get_person_id(self):
+        return self._person_id
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in ein Rating()."""
+        obj = Profile()
+        """print(dictionary)"""
+        obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
+        obj.set_creation_time(dictionary["creation_time"])
+        obj.set_name(dictionary["name"]) #
+        obj.set_age(dictionary["age"])
+        obj.set_adress(dictionary["adress"])
+        obj.set_semester(dictionary["semester"])
+        obj.set_degree_course(dictionary["degree_course"])
+        obj.set_pre_knowledge(dictionary["pre_knowledge"])
+        obj.set_person_id(dictionary["person_id"])
+
+        return obj
+
+
+       
