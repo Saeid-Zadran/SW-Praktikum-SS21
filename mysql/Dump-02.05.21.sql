e34cd071cca6acc4e758670ea576a73a8cf863da
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2021-05-04 16:10:00 +0200

    Dump aktuallisert

--- mysql/Dump-02.05.21.sql
+++ mysql/Dump-02.05.21.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sw-project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sw-project`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: sw-project
