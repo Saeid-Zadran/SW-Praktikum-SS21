92041e59d4f46df451ffdd2b008b886ab535d595
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2021-05-16 03:08:41 +0200

    FontWeight fix

--- frontend/src/components/layout/Start.js
+++ frontend/src/components/layout/Start.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {Button, Container, CssBaseline, Grid, ThemeProvider, Typography, withStyles} from '@material-ui/core';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+  Typography,
+  withStyles,
+} from "@material-ui/core";
 import Theme from "../../Theme";
-import {BrowserRouter as Router, Redirect, Link, Route} from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Link,
+  Route,
+} from "react-router-dom";
 
 class Start extends Component {
-
-
-    render() {
-        return(
-        <div>
-            <center>
-                <Typography>
-                    Wählen Sie ihr Profil aus:
-                </Typography>
-                <Link to='/Start/ProfileList'>
-                    <Button>
-                        Profile
-                    </Button>
-
-            </center>
-        </div>
-);
-    }
+  render() {
+    return (
+      <div>
+        <center>
+          <Typography>Wählen Sie ihr Profil aus:</Typography>
+          <Link to="/Start/ProfileList">
+            <Button>Profile</Button>
+          </Link>
+        </center>
+      </div>
+    );
+  }
 }
 
 export default Start;
-
-
