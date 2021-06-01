57af65946d7cd79771447262036f053243d11e20
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2021-06-01 15:56:47 +0200

    DBMapper PW

--- src/server/db/DBMapper.py
+++ src/server/db/DBMapper.py
@@ -29,7 +29,7 @@ class Mapper (AbstractContextManager, ABC):
             also auf einem Local Development Server. Hierbei stellen wir eine einfache Verbindung zu einer lokal
             installierten mySQL-Datenbank her."""
 
-            self._cnx = connector.connect(user='root', password='Demo123!',
+            self._cnx = connector.connect(user='root', password='Matan$5806',
                                   host='127.0.0.1',
                                   database='sw-project')
 
