c6880346cb999a3020b2cb73d9588894e2a1aeb4
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2021-06-09 00:47:31 +0200

    ProfileList hinzugefügt
    in der StartPage = neuen Button erstellt

--- frontend/src/components/pages/Start.js
+++ frontend/src/components/pages/Start.js
@@ -1,39 +1,54 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {Button,Container,CssBaseline,Grid,ThemeProvider,Typography,withStyles,} from "@material-ui/core";
+import {
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+  Typography,
+  withStyles,
+} from "@material-ui/core";
 import Theme from "../../Theme";
-import {BrowserRouter as Router,Redirect,Link,Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Link,
+  Route,
+} from "react-router-dom";
 
 class Start extends Component {
   render() {
     return (
       <CssBaseline>
-      
         <div>
           <center>
             <Typography>Willkommen auf der Lern Matching </Typography>
-            <Typography>Angemeldet ? Nein  </Typography>
-            <Link to='/StartPage'>
-                <Button variant="contained" color="primary" >
-                  Create Profile
-                </Button>
-            </Link><br></br>
-            <Typography>Angemeldet ? Ja   </Typography>
-            <Link to='/SecondPage'>
-                <Button variant="contained" color="primary" >
-                  Further Use
-                </Button>
-            
-            
-                
-              
-            
-              
+            <br></br>
+            <Typography>Angemeldet ? Nein </Typography>
+            <Link to="/StartPage">
+              <Button variant="contained" color="primary">
+                Create Profile
+              </Button>
+            </Link>
+            <br></br>
+            <br></br>
+            <Typography>Angemeldet ? Ja </Typography>
+            <Link to="/SecondPage">
+              <Button variant="contained" color="primary">
+                Further Use
+              </Button>
+            </Link>
+            <br></br>
+            <br></br>
+            <Typography>Vorschläge</Typography>
+            <Link to="/ProfileList">
+              <Button variant="contained" color="primary">
+                Vorschläge
+              </Button>
             </Link>
           </center>
         </div>
-        
-         
       </CssBaseline>
     );
   }
