438d6ccc8f2e2cd3004833b0c8b71a9e1660a103
TobiasMüllmaier <tm104@hdm-stuttgart.de>
2021-06-27 21:22:38 +0200

    Dump fix

--- mysql/Dump.sql
+++ mysql/Dump.sql
@@ -88,6 +88,7 @@ CREATE TABLE `grouprequest` (
   `is_accepted` tinyint NOT NULL,
   `learngroup_id` int NOT NULL,
   `person_id` int NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `fk_grouprequest_learngroup1_idx` (`learngroup_id`),
   KEY `fk_grouprequest_person1_idx` (`person_id`),
   CONSTRAINT `fk_grouprequest_learngroup1` FOREIGN KEY (`learngroup_id`) REFERENCES `learngroup` (`id`),
@@ -233,4 +234,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-25 20:45:23
+-- Dump completed on 2021-06-27 21:19:36
